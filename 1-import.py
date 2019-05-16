c711ab7e0674390f4e35d5f3b60da1d79e384de5
Brock Tibert <btibert3@gmail.com>
2019-05-16 09:56:15 -0400

    update for py2neo

--- 1-import.py
+++ 1-import.py
@@ -1,49 +1,56 @@
-# import hockey_scraper
-# 
-# # get the season
-# hockey_scraper.scrape_seasons([2018], True)
-
-# # imports
-# # from py2neo import Graph
-# from neo4j import GraphDatabase
-# import requests
-# import pandas as pd
-# 
-# both drivers not working
-# 
-# # # connect
-# # driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
-# # 
-# # # get the 2018/19 gameids
-# # URL = "http://www.nicetimeonice.com/api/seasons/20182019/games"
-# # resp = requests.get(URL)
-# # data = resp.json()
-# # 
-# # # put into pandas df
-# # game_ids = pd.DataFrame(data)
-# # 
-# # # set the constraints
-# # # graph.schema.create_uniqueness_constraint('Game', 'id')
-# # # db.schema.create_uniqueness_constraint('Game', 'id')
-# # with driver.session() as session:
-# #   session.
-# # 
-
 import hockey_scraper
 import requests
 import pandas as pd
-from py2neo import Graph
+from py2neo import Graph, Node, Relationship
 
 # get the 2018/19 gameids
 URL = "http://www.nicetimeonice.com/api/seasons/20182019/games"
 resp = requests.get(URL)
 data = resp.json()
 
-# connect to neo4j
-driver = Graph(user="neo4j", password="password")
-driver.schema.create_uniqueness_constraint("Game", "id")
-
 # list of games
 games = pd.DataFrame(data)
 gids = list(games.gameID)
+
+game_data = scraped_data = hockey_scraper.scrape_games([int(gids[1])], False, data_format='Pandas')
+game_pbp = game_data['pbp']
+game_pbp.to_pickle("hs-game-pbp.pkl")
+
+#-------------
+
+# connect to neo4j
+driver = Graph(user="neo4j", password="password")
+driver.schema.create_uniqueness_constraint("Game", "id")
+driver.schema.create_uniqueness_constraint("Play", "id")
+
+## nhl game data
+GURL = "http://statsapi.web.nhl.com/api/v1/game/{}/feed/live".format(gids[1])
+resp = requests.get(GURL)
+game = resp.json()
+game.keys()
+
+## create the game node
+gid = game['gamePk']
+g = Node("Game", id=gid)
+driver.create(g)
+
+## live data parsing
+index = 0
+plays = game['liveData']['plays']['allPlays']
+play_data = plays[index]
+play = play_data['result']
+play.update(play_data['about']) 
+del play['goals']
+play.update(play_data['about']['goals'])
+play.update(play_data['coordinates'])
+play['id'] = "{}_{}".format(gid, index+1)
+p = Node.cast(play)  ## cast from a dict
+p.add_label("Play") ## add the label I want
+driver.create(p) ## create the node
+r = Relationship(g, "HAD_PLAY", p)
+driver.create(r)
+
+for index, item in enumerate(plays):
+  print(index)
+
 
