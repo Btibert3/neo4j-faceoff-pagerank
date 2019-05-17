8d19237545a3a9b29cfb6caab012adadc83890d2
Brock Tibert <btibert3@gmail.com>
2019-05-17 19:12:21 -0400

    add python code to parse the game and plays

--- 1-import.py
+++ 1-import.py
@@ -12,9 +12,9 @@ data = resp.json()
 games = pd.DataFrame(data)
 gids = list(games.gameID)
 
-game_data = scraped_data = hockey_scraper.scrape_games([int(gids[1])], False, data_format='Pandas')
-game_pbp = game_data['pbp']
-game_pbp.to_pickle("hs-game-pbp.pkl")
+# game_data = scraped_data = hockey_scraper.scrape_games([int(gids[1])], False, data_format='Pandas')
+# game_pbp = game_data['pbp']
+# game_pbp.to_pickle("hs-game-pbp.pkl")
 
 #-------------
 
@@ -34,23 +34,41 @@ gid = game['gamePk']
 g = Node("Game", id=gid)
 driver.create(g)
 
-## live data parsing
-index = 0
+## get the plays
 plays = game['liveData']['plays']['allPlays']
-play_data = plays[index]
-play = play_data['result']
-play.update(play_data['about']) 
-del play['goals']
-play.update(play_data['about']['goals'])
-play.update(play_data['coordinates'])
-play['id'] = "{}_{}".format(gid, index+1)
-p = Node.cast(play)  ## cast from a dict
-p.add_label("Play") ## add the label I want
-driver.create(p) ## create the node
-r = Relationship(g, "HAD_PLAY", p)
-driver.create(r)
 
-for index, item in enumerate(plays):
-  print(index)
+## test code for parsing
+# index = 169
+# play_data = plays[index]
+# play = play_data['result']
+# play.update(play_data['about'])
+# del play['goals']
+# del play['strength']
+# play.update(play_data['about']['goals'])
+# play.update(play_data['coordinates'])
+# play['id'] = "{}_{}".format(gid, index+1)
+# p = Node.cast(play)  ## cast from a dict
+# p.add_label("Play") ## add the label I want
+# driver.create(p) ## create the node
+# r = Relationship(g, "HAD_PLAY", p)
+# driver.create(r)
+
+for index, play_data in enumerate(plays):
+  ## here
+  play = play_data['result']
+  play.update(play_data['about'])
+  if 'goals' in play.keys():
+    del play['goals']    
+  if 'strength' in play.keys():
+    del play['strength']    
+  play.update(play_data['about']['goals'])
+  play.update(play_data['coordinates'])
+  play['id'] = "{}_{}".format(gid, index+1)
+  p = Node.cast(play)  ## cast from a dict
+  p.add_label("Play") ## add the label I want
+  driver.create(p) ## create the node
+  r = Relationship(g, "HAD_PLAY", p)
+  driver.create(r)
+  print("finished {}".format(index))
 
 
